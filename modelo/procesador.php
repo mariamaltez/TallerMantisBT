11f1b4c79184ef3212fb9245e1ff8030a28cd67b
Jhon Obando <jhonser2109@gmail.com>
2017-10-14 05:07:22 -0300

    se hacen modificaciones segun las sugerencias del kiuwan

--- modelo/procesador.php
+++ modelo/procesador.php
@@ -1,5 +1,5 @@
 <?php
-include("db.php");
+include"db.php";
 $controlador = $_GET["controlador"];
 $accion = $_GET["accion"];
 
--- procesa_login.php
+++ procesa_login.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include("modelo/db.php");
+include"modelo/db.php";
 
 $user = $_POST["user_name"];
 $password = $_POST["password"];
