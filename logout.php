0d56142304292c6c2ed6c6dce49abc5473b0b647
Jhon Obando <31368467+0ban2@users.noreply.github.com>
2017-10-15 10:31:17 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -6,12 +6,4 @@ if (!isset($_SESSION['user']) || empty($_SESSION['user'])) {
 }
 session_destroy();
 
-<<<<<<< HEAD
-
 header("Location: login.php?codigo=2");
-
-=======
-
-header('Location: login.php?codigo=2');
-
->>>>>>> 609ccfa458f5b7f7a120bed89c6367d55e87f441
